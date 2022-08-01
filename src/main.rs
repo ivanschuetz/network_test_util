555fa9535fe90eb994a5d5acda55641be8debb2b
Ivan Schuetz <ivanschuetz@gmail.com>
2022-08-01 18:51:42 +0200

    Show success message

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,6 @@ use network_test_util::wasm::reset_and_fund_local_network;
 #[tokio::main]
 async fn main() -> Result<()> {
     reset_and_fund_local_network().await?;
+    log::info!("Network reset and configured.");
     Ok(())
 }
